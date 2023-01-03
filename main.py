9cca86206a9e8f1221f71db4490694b741a82eae
Javad <amiryan.j@gmail.com>
2023-01-03 18:04:01 +0100

    call r3_monitoring in main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import sys
+import time
+from threading import Thread
 from PyQt5.QtWidgets import QApplication
-from python.app_controller import AppController
+from core.app_controller import AppController
+from core.r3_monitoring_client import R3MonitoringClient
+from core.config import CONFIGS
+
+# add this folder to the python path
+sys.path.append(".")
 
 
 def main():
@@ -8,6 +16,15 @@ def main():
 
     app_controller = AppController(app)
     app_controller.make_system_tray_menu()
+    r3_monitoring = R3MonitoringClient(CONFIGS)
+
+    # run on a separate thread
+    def r3_monitoring_thread_run():
+        while True:
+            r3_monitoring.step()
+            time.sleep(2)
+    r3_monitoring_thread = Thread(target=r3_monitoring_thread_run)
+    r3_monitoring_thread.start()
 
     app.exec_()
 
