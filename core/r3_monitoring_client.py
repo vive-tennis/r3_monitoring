3823c7e1c0464d468fea1edfe3a55fb2283baf4d
Javad <amiryan.j@gmail.com>
2023-01-03 18:03:04 +0100

    rename python dir to core!

--- core/r3_monitoring_client.py
+++ core/r3_monitoring_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import signal
 import importlib
 import os
 import time
@@ -9,8 +9,7 @@ import json
 import paho.mqtt.client as paho
 import yaml
 
-from serialization import ros2dict
-from config import CONFIGS
+from core.serialization import ros2dict
 
 
 if os.environ.get("ROS_VERSION") == "1":
@@ -21,29 +20,30 @@ else:
 
 
 class R3MonitoringClient:
-    def __init__(self, name="ros_r3_monitoring", protocol_type="udp"):
+    def __init__(self, configs, name="ros_r3_monitoring"):
+        self.configs = configs
         self.all_topics = {}
         self.local_subs = {}
-        self.protocol_type = protocol_type.lower()
-        self.server_images_address_port = CONFIGS.SERVER_IP, CONFIGS.IMAGE_PORT
+        self.protocol_type = self.configs.SOCKET_TYPE.lower()
+        self.server_images_address_port = self.configs.SERVER_IP, self.configs.IMAGE_PORT
 
         self.black_list_topics_name = []
         self.black_list_topics_type = []
 
         if self.protocol_type == "tcp":
-            self.server_address_port = CONFIGS.SERVER_IP, CONFIGS.TCP_PORT
+            self.server_address_port = self.configs.SERVER_IP, self.configs.TCP_PORT
             self.socket_type = socket.SOCK_STREAM
         elif self.protocol_type == "udp":
-            self.server_address_port = CONFIGS.SERVER_IP, CONFIGS.UDP_PORT
+            self.server_address_port = self.configs.SERVER_IP, self.configs.UDP_PORT
             self.socket_type = socket.SOCK_DGRAM
         elif self.protocol_type == "mqtt":
-            self.server_address_port = CONFIGS.SERVER_IP, CONFIGS.MQTT_PORT
+            self.server_address_port = self.configs.SERVER_IP, self.configs.MQTT_PORT
             pass
         else:
             raise ValueError(f"Invalid socket type: {self.protocol_type}")
 
         start_connecting_time = time.time()
-        while time.time() - start_connecting_time < CONFIGS.CONNECTION_TIMEOUT:
+        while time.time() - start_connecting_time < self.configs.CONNECTION_TIMEOUT:
             try:
                 if self.protocol_type in ["tcp", "udp"]:
                     self.socket = socket.socket(family=socket.AF_INET, type=self.socket_type)
@@ -55,8 +55,8 @@ class R3MonitoringClient:
                 if self.protocol_type == "mqtt":
                     self.socket = paho.Client("control1")  # create client object
                     self.socket.on_publish = lambda a, b, c: None  # assign function to callback
-                    self.socket.username_pw_set(CONFIGS.ACCESS_TOKEN)  # access token from thingsboard device
-                    self.socket.connect(CONFIGS.IP, CONFIGS.MQTT_PORT, keepalive=60)  # establish connection
+                    self.socket.username_pw_set(self.configs.ACCESS_TOKEN)  # access token from thingsboard device
+                    self.socket.connect(self.configs.IP, self.configs.MQTT_PORT, keepalive=60)  # establish connection
 
                 print("R3MonitoringClient: connected to server successfully!")
                 break
@@ -64,7 +64,7 @@ class R3MonitoringClient:
                 print(e)
                 time.sleep(3)
 
-        self.buffer_size = CONFIGS.BUFFER_SIZE
+        self.buffer_size = self.configs.BUFFER_SIZE
         self.last_time_topic_sent = {}
         try:
             rospy.init_node(name)
@@ -72,6 +72,22 @@ class R3MonitoringClient:
         except Exception as e:
             print("Error (R3MonitoringClient): ", e)
             # self.socket.close()
+
+        signal.signal(signal.SIGUSR1, self.signal_handler)
+        signal.signal(signal.SIGUSR2, self.signal_handler)
+        signal.signal(signal.SIGHUP, self.signal_handler)
+        self.pause_program = False
+
+    def signal_handler(self, sig, frame):
+        if sig == signal.SIGUSR1:
+            self.pause_program = True
+            print("Pause program")
+        elif sig == signal.SIGUSR2:
+            self.pause_program = False
+            print("Resume program")
+        elif sig == signal.SIGHUP:
+            self.load_black_list()
+            print("Reload black list topics")
 
     def load_black_list(self):
         try:
@@ -86,12 +102,14 @@ class R3MonitoringClient:
             return
 
     def on_ros_msg(self, ros_msg, topic_info):
+        if self.pause_program:
+            return
         topic_name, topic_type = topic_info
 
         # convert ROS message into a dict and get it ready for serialization
         ros_msg_dict = ros2dict(ros_msg)
 
-        if self.protocol_type == "udp" and CONFIGS.SEND_IMAGES and "_data_jpeg" in ros_msg_dict.keys():
+        if self.protocol_type == "udp" and self.configs.SEND_IMAGES and "_data_jpeg" in ros_msg_dict.keys():
             img = ros_msg_dict['_data_jpeg']
             mtu = 1000
             n = len(img) // mtu
@@ -112,7 +130,7 @@ class R3MonitoringClient:
         ros_msg_dict["host_name"] = socket.gethostname()
 
         # don't send messages faster than 5 Hz
-        if ts - self.last_time_topic_sent.get(topic_name, 0) < CONFIGS.SEND_FREQ:
+        if ts - self.last_time_topic_sent.get(topic_name, 0) < self.configs.SEND_FREQ:
             return
         self.last_time_topic_sent[topic_name] = ts
 
@@ -192,31 +210,16 @@ class R3MonitoringClient:
             # self.socket.close()
 
 
-if __name__ == '__main__':
-    ros_logger = R3MonitoringClient(protocol_type=CONFIGS.SOCKET_TYPE)
-    ros_logger.load_black_list()
-
-    import signal
-    pause_program = False
-
-    def signal_handler(sig, frame):
-        global pause_program
-        if sig == signal.SIGUSR1:
-            pause_program = True
-            print("Pause program")
-        elif sig == signal.SIGUSR2:
-            pause_program = False
-            print("Resume program")
-        elif sig == signal.SIGHUP:
-            ros_logger.load_black_list()
-            print("Reload black list topics")
-
-    signal.signal(signal.SIGUSR1, signal_handler)
-    signal.signal(signal.SIGUSR2, signal_handler)
-    signal.signal(signal.SIGHUP, signal_handler)
+def main():
+    from core.config import CONFIGS
+    r3_monitoring = R3MonitoringClient(CONFIGS)
+    r3_monitoring.load_black_list()
 
     while True:
-        if not pause_program:
-            ros_logger.step()
+        r3_monitoring.step()
         time.sleep(2)
+
+
+if __name__ == "__main__":
+    main()
 
