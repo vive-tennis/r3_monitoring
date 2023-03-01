695373653c2c3b292515d7c8d3f845435398cfaf
mojtaba_sji <mojtabashojailine@yahoo.com>
2023-03-01 14:55:50 +0330

    r3_monitoring_user.py: remove comment lines -> using get_msg_class

--- r3_monitoring_user.py
+++ r3_monitoring_user.py
@@ -3,14 +3,6 @@ import json
 import rospy
 from core.r3_monitoring_client import get_msg_class
 
-from geometry_msgs.msg import Quaternion, Pose, Point
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
-from rosgraph_msgs.msg import Log
-from sensor_msgs.msg import Imu, Image, BatteryState
-from sensor_msgs.msg import Range
-from visualization_msgs.msg import Marker
-
 try:
     from std_msgs_stamped.msg import StringStamped, Float64Stamped, Float64MultiArrayStamped
 except ImportError:
@@ -44,54 +36,6 @@ def on_message(client, userdata, message):
     # print(f'Received message: {json_msg})')
     try:
         if json_msg['_topic_name'] not in subscribed:
-            # if json_msg['_topic_type'] == 'rosgraph_msgs/Log':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Log, queue_size=10)
-            #     mesg = Log()
-            # elif json_msg['_topic_type'] == 'nav_msgs/Odometry':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Odometry, queue_size=10)
-            #     mesg = Odometry()
-            # elif json_msg['_topic_type'] == 'geometry_msgs/Twist':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Twist, queue_size=10)
-            #     mesg = Twist()
-            # elif json_msg['_topic_type'] == 'geometry_msgs/Pose':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Pose, queue_size=10)
-            #     mesg = Pose()
-            # elif json_msg['_topic_type'] == 'geometry_msgs/Point':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Point, queue_size=10)
-            #     mesg = Point()
-            # elif json_msg['_topic_type'] == 'geometry_msgs/Quaternion':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Quaternion, queue_size=10)
-            #     mesg = Quaternion()
-            # elif json_msg['_topic_type'] == 'sensor_msgs/Imu':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Imu, queue_size=10)
-            #     mesg = Imu()
-            # elif json_msg['_topic_type'] == 'sensor_msgs/Image':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Image, queue_size=10)
-            #     mesg = Image()
-            # elif json_msg['_topic_type'] == 'sensor_msgs/BatteryState':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], BatteryState, queue_size=10)
-            #     mesg = BatteryState()
-            # elif json_msg['_topic_type'] == 'sensor_msgs/Range':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Range, queue_size=10)
-            #     mesg = Range()
-            # elif json_msg['_topic_type'] == 'std_msgs/String':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], StringStamped, queue_size=10)
-            #     mesg = StringStamped()
-            # elif json_msg['_topic_type'] == 'std_msgs/Float64':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Float64Stamped, queue_size=10)
-            #     mesg = Float64Stamped()
-            # elif json_msg['_topic_type'] == 'std_msgs_stamped/Float64Stamped':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Float64Stamped, queue_size=10)
-            #     mesg = Float64Stamped()
-            # elif json_msg['_topic_type'] == 'std_msgs/Float64MultiArray':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Float64MultiArrayStamped, queue_size=10)
-            #     mesg = Float64MultiArrayStamped()
-            # elif json_msg['_topic_type'] == 'visualization_msgs/Marker':
-            #     pub = rospy.Publisher(json_msg['_topic_name'], Marker, queue_size=10)
-            #     mesg = Marker()
-            # else:
-            #     print(f'Unknown topic type: {json_msg["_topic_type"]}')
-            #     return
 
             mesg = get_msg_class(json_msg['_topic_name'])
             pub = rospy.Publisher(json_msg['_topic_name'], mesg, queue_size=10)
