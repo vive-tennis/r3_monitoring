882ddb44b1332cc27d879e172c5b35e5ee29caf8
Javad <amiryan.j@gmail.com>
2023-06-11 15:01:31 +0200

    added missing setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='r3_monitoring',
+    version='0.1.0',
+    packages=['r3_monitoring'],
+    # scripts=['scripts/my_script.py'],
+    install_requires=['rospy'],
+    author='Javad Amirian',
+    author_email='amiryan.j@gmail.com',
+    description='Monitoring Robot Remotely in Realtime',
+    license='MIT License',
+    keywords='ROS',
+    url='https://github.com/vive-tennis/r3-monitoring-client',
+)
