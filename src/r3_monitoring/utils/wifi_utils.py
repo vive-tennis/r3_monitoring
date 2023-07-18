349fa575433a7fbc19e4ba3a0a0b6d1631d6d50a
Javad <amiryan.j@gmail.com>
2023-07-18 19:31:16 +0200

    new implementation for wifi_utils::get_wifi_interface()

--- src/r3_monitoring/utils/wifi_utils.py
+++ src/r3_monitoring/utils/wifi_utils.py
@@ -1,12 +1,26 @@
 import os
+import subprocess
+
+
+# def get_wifi_interface():
+#     tmp_filename = "/tmp/wifi_device_tmp.txt"
+#     os.system("iw dev | grep Interface | awk '{print $2}' > %s" % tmp_filename)
+#     with open(tmp_filename, "r") as f:
+#         wifi_name = f.readline()
+#     return wifi_name.strip()
 
 
 def get_wifi_interface():
-    tmp_filename = "/tmp/wifi_device_tmp.txt"
-    os.system("iw dev | grep Interface | awk '{print $2}' > %s" % tmp_filename)
-    with open(tmp_filename, "r") as f:
-        wifi_name = f.readline()
-    return wifi_name.strip()
+    try:
+        output = subprocess.check_output(['nmcli', 'device', 'show'], universal_newlines=True)
+        lines = output.strip().split('\n')
+        for ii, line in enumerate(lines):
+            if 'GENERAL.TYPE:' in line and 'wifi' in line and ii > 0 and 'GENERAL.DEVICE:' in lines[ii-1]:
+                _, interface = lines[ii-1].strip().split(':')
+                return interface.strip()
+    except subprocess.CalledProcessError:
+        print("Error occurred while finding WiFi devices.")
+        return ""
 
 
 def get_wifi_ssid_name(interface_name):
